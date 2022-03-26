da9972fd50c250019f863a7c001f5a61ab86e5cd
Lucas Eduardo dos Santos de Souza <84149268+LucasEduardoSS@users.noreply.github.com>
2022-03-26 20:59:55 -0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -10,10 +10,20 @@ def line(line_length, msg='-'):
 
 
 def validated_input(msg, value_type=int, sep_line=False):
+    """
+    Valida um input corrigindo possíveis erros. Pode receber todos os tipos de valores.
+    :param msg: mensagem apresentada ao usuário.
+    :param value_type: tipo de valor a ser validado.
+    :param sep_line: escreve uma linha de separação.
+    :return: retorna o valor do usuário para a variável.
+    """
+
     value = 0
     while value == 0:
         try:
             value = value_type(input(msg))
+            if value_type == str:
+                value = str(value).strip().upper()[0]
         except ValueError:
             print('\33[31mValor incorreto! tente novamente.\33[m')
         except TypeError:
@@ -26,3 +36,16 @@ def validated_input(msg, value_type=int, sep_line=False):
 def value_limit(min_limit, max_limit, value, msg):
     while max_limit < value < min_limit:
         validated_input(msg)
+
+
+def calculate_current():
+    potencia = float(validated_input('Informe a potência do(s) aparelho(s): '))
+    tensao = float(validated_input('Tensão do circuito: '))
+    print(f'A corrente do circuito é de {potencia / tensao:.2f}A.')
+    corrente = potencia / tensao
+    line(102)
+    return corrente
+
+
+def get_current():
+    corrente = validated_input('Corrente do circuito')
--- main.py
+++ main.py
@@ -2,19 +2,18 @@
 
 # IMPORTAÇÕES
 from time import sleep
-from functions import title, line, validated_input
-from tabelas import metodo
+from functions import title, line, validated_input, calculate_current
+from tabelas import tabela_36, tabela_42, disjuntores, materiais
 
 # DECLARAÇÕES
-fca = fcc = fct = f = k = h = corrente = cccfc = resistencia = 0
-condutores = [
-    {'Bitola': '1.5 mm', 'Cap. Condução': 17.5},
-    {'Bitola': '2.5 mm', 'Cap. Condução': 24},
-    {'Bitola': '4 mm', 'Cap. Condução': 32},
-    {'Bitola': '6 mm', 'Cap. Condução': 41},
-    {'Bitola': '10 mm', 'Cap. Condução': 57},
-    {'Bitola': '16 mm', 'Cap. Condução': 76}
-]
+fcc = is_enough = user_choice = is_to_continue = cond_crg = corrente = cccfc = resistencia = 0
+cond_min_index = 0
+
+
+def var_status():
+    print('\n<VARIABLES STATUS>')
+    print(f'fator de correção de condução: {fcc:.2f}')
+
 
 # CÓDIGO PRINCIPAL
 title(" Programa pré-definições do circuito ", 102)
@@ -29,7 +28,7 @@ while True:
 \33[36m4 - Finalizar Programa\33[m
 ''')
 
-    k = validated_input('Digite aqui: ')
+    user_choice = validated_input('Digite aqui: ')
 
     '''
     DIMENSIONAMENTO DO CABO
@@ -39,10 +38,10 @@ while True:
     condução de corrente pelos condutores.
     '''
 
-    if k == 1:
+    if user_choice == 1:
         title(" Dimensionamento do condutor ", 102)
 
-        # sessão onde é informado se é um circuito de luz ou força
+        # Decide se o circuito é de força ou de luz
         circuito = str(input('Luz ou Força? ')).strip().upper()[0]
 
         # Menu Tipo de Circuito
@@ -56,8 +55,14 @@ while True:
 \33[36m6 - Trifásico com neutro\33[m
 ''')
 
-        tipo_circuito = int(input('Digite aqui: '))
+        tipo_circ = int(input('Digite aqui: '))
         line(102)
+
+        if tipo_circ in [1, 2, 3]:
+            cond_crg = 2
+
+        elif tipo_circ in [4, 5, 6]:
+            cond_crg = 3
 
         # Recebe o valor da corrente do circuito
         corrente = float(input('Corrente do Circuito, se não tiver, digite 0: '))
@@ -65,11 +70,7 @@ while True:
 
         # Calcula a corrente caso ela não seja informada pelo usuário
         if corrente == 0:
-            potencia = float(input('Informe a potência do(s) aparelho(s): '))
-            tensao = float(input('Informe a tensão do(s) aparelho(s): '))
-            print(f'A corrente do circuito é de {potencia / tensao:.2f}A.')
-            corrente = potencia / tensao
-            line(102)
+            corrente = calculate_current()
 
         # Menu Referência do método de condicionamento do circuito conforme tabela 33
         print('''Indique o método de referência conforme tabela 33:
@@ -82,31 +83,13 @@ seção circular embutido em alvenaria - B1\33[m
         line(102)
 
         if metodo_de_referencia == 7:
-            temp = int(input('Informe a temperatura ambiente em °C (padrão 35°C): '))
+            red_met_index = 2
 
-            if temp == 35:
-                line(102)
-                material = str(
-                    input('Informe qual o material usado no revestimento dos condutores [PVC/EPR ou XLPE]: ')
-                )
+        # Recebe o número de circuitos instalados no conduíte em questão
+        qnt_circuitos = int(input('Quantidade de circuitos presente no conduite: '))
 
-                if material == 'P'.strip().upper()[0]:
-                    fct = 0.94
-
-            # Sessão onde o usuário informa o número de circuitos instalados no conduíte em questão
-
-            line(102)
-            qnt_circuitos = int(input('Informe a quantidade de circuitos presente no conduite : '))
-
-            if qnt_circuitos > 5:  # Alteração do número de circuitos do conduíte
-                print('Recomenda-se utilizar no máximo 5 circuitos por conduíte')
-                opcao = str(input('Deseja alterar a quantidade? ')).strip()[0]
-
-                if opcao in 'Ss':  # Condição de alteração do número de circuitos no conduíte
-                    qnt_circuitos = int(input('Informe uma nova quantidade de circuitos presentes no conduite: '))
-
-            # Menu Métodos de Agrupamento / Tabela 42
-            print('-' * 102, '''\nForma de agrupamento dos condutores segundo a tabela 42 da nbr 5410:
+        # Menu Métodos de Agrupamento / Tabela 42
+        print('-' * 102, '''\nForma de agrupamento dos condutores segundo a tabela 42 da nbr 5410:
 
 \33[33m1 - Em feixe: ao ar livre ou sobre superfície; embutido; em conduto fechado
 \33[36m2 - Camada única sobre parede, piso, ou em bandeja não perfurada ou prateleira
@@ -115,153 +98,28 @@ seção circular embutido em alvenaria - B1\33[m
 \33[33m5 - Camada única sobre leito, suporte etc\33[m
 ''')
 
-            ref = int(input('Digite aqui: '))
+        ref_agrupamento = int(input('Digite aqui: '))
 
-            cr_factor = (1, 0.8, 0.7, 0.65, 0.6)
-            fca = cr_factor[ref - 1]
-            fcc = fct * fca  # Operação que obtém o fator de correção do circuito
+        fcc = tabela_42[ref_agrupamento - 1][qnt_circuitos - 1]
 
-            line(102)
+        var_status()
 
-            # DIMENSIONAMENTO DE CIRCUITO DE LUZ
+        line(102)
 
-            if circuito == 'L':
+        if circuito == 'L':
+            cond_min_index = 3
+        elif circuito == 'F':
+            cond_min_index = 4
 
-                if tipo_circuito == 1 or 2 or 3:
-                    # ccc significa capacidade de condução do condutor em empares, no caso um condutor de 1,5 mm
-                    ccc = 17.5
-                    cccfc = ccc * fcc
-                    print(
-                        'A capacidade de condução de corrente do circuito de 1,5 mm^2'
-                        f' com o fator de correção é de {cccfc:.2f}A'
-                    )
-
-                    if corrente <= cccfc:
-                        print('O cabo de 1.5 mm^2 é capaz de alimentar este circuito')
-                        f = 1
-
-                    else:
-                        print('o cabo de 1.5 mm^2 não capaz de alimentar este circuito')
-                        f = 0
-
-                        # Sessão para determinar o condutor caso o mínimo não seja o suficiente
-
-                        while f == 0:
-                            # Menu Condutores
-                            print('-' * 102, '''\nIndique uma sessão maior :
-
-a - 2,5 mm^2
-b - 4 mm^2
-c - 6 mm^2
-d - 10 mm^2
-                            ''')
-
-                            s = str(input('Digite aqui : ')).strip().lower()[0]
-                            line(102)
-
-                            if s == 'a':
-                                cccfc = 24 * fcc
-                                print(
-                                    'A capacidade de condução de corrente do circuito de 2,5 mm^2'
-                                    f' com o fator de correção é de {cccfc:.2f}A'
-                                )
-
-                            elif s == 'b':
-                                cccfc = 32 * fcc
-                                print('A capacidade de condução de corrente do circuito de '
-                                      f'4 mm^2 com o fator de correção é de {cccfc:.2f}A')
-
-                            elif s == 'c':
-                                cccfc = 41 * fcc
-                                print('A capacidade de condução de corrente do circuito de '
-                                      f'6 mm^2 com o fator de correção é de {cccfc:.2f}A')
-
-                            elif s == 'd':
-                                cccfc = 57 * fcc
-                                print('A capacidade de condução de corrente do circuito de '
-                                      f'10 mm^2 com o fator de correção é de {cccfc:.2f}A')
-
-                            if cccfc < corrente:
-                                print('Este cabo não é capaz de suportar a corrente do circuito.')
-                                line(102)
-                                f = 0
-
-                            else:
-                                f = 1
-                        print('Este cabo é capaz de suportar a corrente.')
-                        line(102)
-
-            # Sessão para determinar a dimensão do condutor caso o circuito seja de força
-
-            if circuito == 'F':
-                if tipo_circuito == 1 or 2 or 3:
-                    ccc = 24
-                    cccfc = ccc * fcc
-                    print(
-                        'A capacidade de condução de corrente do condutor de 2,5 mm^2'
-                        f' com o fator de correção é de {cccfc:.2f}A'
-                    )
-
-                    if corrente <= cccfc:
-                        print('O cabo de 2,5 mm^2 é capaz de alimentar este circuito')
-                        f = 1
-
-                    else:
-                        print('o cabo de 2,5 mm^2 não capaz de alimentar este circuito')
-                        line(102)
-                        f = 0
-
-                        while f == 0:
-                            # Menu Condutores
-                            print('''Indique uma sessão maior :
-
-A - 4 mm^2
-B - 6 mm^2
-C - 10 mm^2
-D - 16 mm^2
-                            ''')
-
-                            s = str(input('Digite aqui : ')).strip().lower()[0]
-                            line(102)
-
-                            if s == 'a':
-                                cccfc = fcc * 32
-                                print(
-                                    'A capacidade de condução de corrente do circuito de 4 mm^2'
-                                    f'com o fator de correção é de {cccfc:.2f}A.')
-
-                            if s == 'b':
-                                cccfc = fcc * 41
-                                print(
-                                    'A capacidade de condução de corrente do circuito de 6 mm^2'
-                                    f' com o fator de correção é de {cccfc:.2f}A')
-
-                            if s == 'c':
-                                cccfc = fcc * 57
-                                print(
-                                    'A capacidade de condução de corrente do circuito de 10 mm^2'
-                                    f'com o fator de correção é de {cccfc:.2f}A.')
-
-                            if s == 'd':
-                                cccfc = fcc * 76
-                                print(
-                                    'A capacidade de condução de corrente do circuito de 16 mm^2'
-                                    f' com o fator de correção é de {cccfc:.2f}A.')
-
-                            if s not in 'abcd':
-                                print('Comando inválido. tente novamente.')
-                                f = 0
-
-                            if cccfc < corrente:
-                                print('Este cabo não é capaz de suportar a corrente do circuito.')
-                                print('-' * 102)
-                                f = 0
-
-                            else:
-                                f = 1
-
-                        print('Este cabo é capaz de suportar a corrente')
-                        line(102)
+        # Dimensiona o condutor
+        for pos, condutor in enumerate(tabela_36[f'CCC{cond_crg}'][cond_min_index:]):
+            cccfc = condutor * fcc
+            if corrente <= cccfc:
+                print(f'O condutor de {tabela_36["Cond"][pos + cond_min_index]} mm^2 ', end='')
+                print(f'é capaz de alimentar este circuito.')
+                print(f'Sua capacidade de condução corrigida é de {cccfc:.2f}A.')
+                line(102)
+                break
 
     '''
     CALCULO SESSÃO TRANSVERSAL MÍNIMA DO CONDUTOR
@@ -271,55 +129,45 @@ D - 16 mm^2
     matemáticas apresentadas em vídeos tutoriais.
     '''
 
-    if k == 2:
+    if user_choice == 2:
         title(" I  Sessão transversal do condutor para quedas de tensão  I ", 102)
+        print(
+            '''
+        ATENÇÃO
+        
+        Recomenda-se o uso desta função para dimensionar circuitos de alta tensão,
+        como aqueles utilizados em oficinas, grandes edificações, centros de pesquisa, etc.
+        '''
+        )
 
-        if corrente != 0 and cccfc != 0:
+        # Escolhe se será usada a corrente calculada
+        if cccfc != 0:
+            is_to_continue = validated_input('Deseja utilizar as corrente dos cálculos anteriores? ', str, True)
 
-            while h == 0:
-                h = str(input('Deseja utilizar as corrente dos cálculos anteriores? ')).strip().upper()[0]
-                line(102)
+            if is_to_continue == 'N':
+                corrente = validated_input('Informe a nova corrente do circuito : ', float, True)
+                cccfc = validated_input('Informe a nova capacidade de condução corrigida: ', float, True)
 
-                if h == 'S':
-                    print('Com base nas correntes informada nos cálculos anteriores,')
-                    line(102)
+            elif is_to_continue == 'S':
+                print('Com base na corrente informada nos cálculos anteriores,\n', end='')
 
-                # Calcula a corrente caso o usuário não a tenha
-                elif h == 'N':
-                    corrente = float(input('Informe a nova corrente do circuito : '))
-                    line(102)
-                    cccfc = float(input(
-                        'Informe a nova capacidade de condução de corrente dos condutores com fator de correção : '))
-                    line(102)
-
-                else:
-                    print('Comando inválido, tente novamente.')
-                    line(102)
-                    h = 0
-
+        # Recebe a corrente do circuito
         if corrente == 0:
-            corrente = float(input('Informe a corrente do circuito : '))
+            corrente = float(input('Informe a corrente do circuito: '))
             line(102)
 
-        distancia = float(input('Informe o comprimento do circuito em metros.'
-                                'Em caso de circuito ramificado, leve em conta a ramificação mais longa : '))
-        line(102)
+        print('''Informe o comprimento do circuito em metros. Em caso de circuito ramificado
+leve em conta aquele com maior valor.
+''')
 
-        material_condutor = str(input('Informe o material do condutor [ Cobre / Alumínio / Ouro ] : ')).strip().upper()[
-            0]
-        line(102)
+        # Recebe o valor do comprimento, material condutor e tensão do circuito
+        distancia = validated_input('Comprimento: ', float, True)
+        material_condutor = validated_input('Material do condutor [ Cobre / Alumínio / Ouro ]: ', str, True)
+        tensao = validated_input('Informe o valor da tensão do circuito: ', int, True)
 
-        tensao = float(input('Informe o valor da tensão do circuito : '))
-        line(102)
-
-        if material_condutor == 'C':
-            resistencia = 0.0172
-
-        elif material_condutor == 'A':
-            resistencia = 0.0282
-
-        elif material_condutor == 'O':
-            resistencia = 0.0244
+        for pos, material in enumerate(materiais['Nome']):
+            if material_condutor == material:
+                resistencia = materiais['Resistências'][pos]
 
         s = ((resistencia * distancia * corrente * 2) / (4 * tensao)) * 100
 
@@ -335,78 +183,33 @@ D - 16 mm^2
     correção.
     '''
 
-    if k == 3:
-        title(" I  Determinação do disjuntor / Linha Easy9 Schneider I ", 102)
+    if user_choice == 3:
+        title(" Determinação do disjuntor / Linha Easy9 Schneider ", 102)
 
-        if corrente != 0 and cccfc != 0:
+        # Escolhe se será usada a corrente calculada
+        if cccfc != 0:
+            is_to_continue = validated_input('Deseja utilizar as corrente dos cálculos anteriores? ', str, True)
 
-            while True:
-                h = str(input('Deseja utilizar as corrente dos cálculos anteriores? ')).strip().upper()[0]
-                line(102)
+            if is_to_continue == 'N':
+                corrente = validated_input('Nova corrente do circuito: ', float, True)
+                cccfc = validated_input('Nova capacidade de condução corrigida: ', float)
 
-                if h == 'S':
-                    print('Com base na corrente informada nos cálculos anteriores,', end='')
-                    break
+            elif is_to_continue == 'S':
+                print('Com base na corrente informada nos cálculos anteriores,\n', end='')
 
-                elif h == 'N':
-                    corrente = float(input('Informe a nova corrente do circuito : '))
-                    line(102)
-                    cccfc = float(input(
-                        'Informe a nova capacidade de condução de corrente dos condutores com fator de correção : '))
-                    line(102)
-                    break
+        # Dimensiona o disjuntor
+        for disjuntor in disjuntores:
+            if corrente <= disjuntor <= cccfc:
+                print(f'O disjuntor de {disjuntor}A é o ideal para este circuito.')
 
-                else:
-                    print('Comando inválido, tente novamente.')
-                    line(102)
-            h = 0
-
-        elif corrente == 0 and cccfc == 0:
-            corrente = float(input('Informe a corrente do circuito : '))
-            line(102)
-            cccfc = float(input('Informe a capacidade de condução de corrente dos condutores com fator de correção : '))
-            line(102)
-
-        if corrente <= 2 <= cccfc:
-            print('O disjuntor de 2A é o ideal para este circuito.')
-
-        elif corrente <= 4 <= cccfc:
-            print('O disjuntor de 4A é o ideal para este circuito.')
-
-        elif corrente <= 6 <= cccfc:
-            print('O disjuntor de 6A é o ideal para este circuito.')
-
-        elif corrente <= 10 <= cccfc:
-            print('O disjuntor de 10A é o ideal para este circuito.')
-
-        elif corrente <= 16 <= cccfc:
-            print('O disjuntor de 16A é o ideal para este circuito.')
-
-        elif corrente <= 20 <= cccfc:
-            print('O disjuntor de 20A é o ideal para este circuito.')
-
-        elif corrente <= 25 <= cccfc:
-            print('O disjuntor de 25A é o ideal para este circuito.')
-
-        elif corrente <= 32 <= cccfc:
-            print('O disjuntor de 32A é o ideal para este circuito.')
-
-        elif corrente <= 40 <= cccfc:
-            print('O disjuntor de 40A é o ideal para este circuito.')
-
-        elif corrente <= 50 <= cccfc:
-            print('O disjuntor de 50A é o ideal para este circuito.')
-
-        elif corrente > cccfc:
-            print('Este condutor não é capaz de alimentar o circuito ')
-
-        line(102)
+            else:
+                print('Não há disjuntor disponível para este circuito.')
 
     # Encerra o programa
-    elif k == 4:
+    elif user_choice == 4:
         break
 
-    else:
+    elif user_choice not in [1, 2, 3, 4]:
         print('Comando inválido. tente novamente.')
 
 print('Finalizando o programa...'), sleep(1)
